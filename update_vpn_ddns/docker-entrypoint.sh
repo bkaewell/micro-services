67417938823ecfcd81efd2c126ff25d6685fbfc0
Brian Kaewell <briankaewell@gmail.com>
2025-08-26 17:18:19 -0400

    Refactor docker setup to replace requirements.txt with Poetry

--- update_vpn_ddns/docker-entrypoint.sh
+++ update_vpn_ddns/docker-entrypoint.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
-
-# Exit on error
 set -e
 
-# Check if CRON_ENABLED is set (docker-compose.yaml loads it automatically)
-CRON_ENABLED=${CRON_ENABLED:-false}  # Default to false if not set
+CRON_ENABLED=${CRON_ENABLED:-false}
 
 if [ "$CRON_ENABLED" = "true" ]; then
   echo "Starting cron daemon..."
-
-  # Ensure cron logs are written to a file
   touch /var/log/cron.log
-  tail -f /var/log/cron.log &  # Keep logs visible
-
-  # Start cron service in the foreground, keep Docker container alive
+  tail -f /var/log/cron.log &
   cron -f
 else
   echo "CRON disabled. Running script once."
-  /usr/local/bin/python /app/src/ip_upload.py
+  poetry run python /app/src/update_vpn_ddns.py
 fi
+
+
+# #!/bin/bash
+
+# # Exit on error
+# set -e
+
+# # Check if CRON_ENABLED is set (docker-compose.yaml loads it automatically)
+# CRON_ENABLED=${CRON_ENABLED:-false}  # Default to false if not set
+
+# if [ "$CRON_ENABLED" = "true" ]; then
+#   echo "Starting cron daemon..."
+
+#   # Ensure cron logs are written to a file
+#   touch /var/log/cron.log
+#   tail -f /var/log/cron.log &  # Keep logs visible
+
+#   # Start cron service in the foreground, keep Docker container alive
+#   cron -f
+# else
+#   echo "CRON disabled. Running script once."
+#   /usr/local/bin/python /app/src/ip_upload.py
+# fi
