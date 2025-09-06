c750d617d018850ee88933ceba60c40e7f47f9d7
Brian Kaewell <briankaewell@gmail.com>
2025-09-06 11:48:32 -0400

    Add is_invalid_ip unit test with realistic test cases

--- update_vpn_ddns/tests/test_ip_utils.py
+++ update_vpn_ddns/tests/test_ip_utils.py
@@ -1,9 +1,19 @@
-
 import pytest
 from update_vpn_ddns.update_vpn_ddns import is_valid_ip
 
+# Google Public DNS
 def test_valid_ipv4():
-    assert is_valid_ip("192.168.0.1", "ipv4") is True
+    assert is_valid_ip("8.8.8.8", "ipv4") is True
 
+def test_invalid_ipv4():
+    assert is_valid_ip("777.777.777.777", "ipv4") is False
 
+# Google Public DNS
+def test_valid_ipv6():
+    assert is_valid_ip("2001:4860:4860::8888", "ipv6") is True
 
+def test_invalid_ipv6():
+    assert is_valid_ip("8.8.8.8", "ipv6") is False
+
+def test_invalid_ip_version():
+    assert is_valid_ip("8.8.8.8", "ipv7") is False
