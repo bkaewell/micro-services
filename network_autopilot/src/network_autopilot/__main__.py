8afbb575acbf7d61b72e4fedee212f5c8c8a574b
Brian Kaewell <briankaewell@gmail.com>
2025-10-24 18:50:37 -0400

    Add main loop design that runs core network_autopilot engine once per minute

--- network_autopilot/src/network_autopilot/__main__.py
+++ network_autopilot/src/network_autopilot/__main__.py
@@ -1,4 +1,12 @@
-from .network_autopilot import main
+import time
+from network_autopilot import network_autopilot
+
+CHECK_INTERVAL = 60 #seconds
+
+def main_loop():
+    while True:
+        network_autopilot.run_cycle()
+        time.sleep(CHECK_INTERVAL)
 
 if __name__ == "__main__":
-    main()
+    main_loop()
