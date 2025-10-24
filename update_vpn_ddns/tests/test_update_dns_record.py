447cadca20a45aa04a970b49fbb03ceadabf1ff3
Brian Kaewell <briankaewell@gmail.com>
2025-10-24 09:55:43 -0400

    Add pytest for update dns record logic

--- update_vpn_ddns/tests/test_update_dns_record.py
+++ update_vpn_ddns/tests/test_update_dns_record.py
@@ -29,3 +29,97 @@
 #     # Call your function
 #     updated = update_dns_record(cloudflare_config, "5.6.7.8")
 #     assert updated is True
+
+
+import pytest
+import responses
+
+from update_vpn_ddns.update_vpn_ddns import update_dns_record
+
+def test_update_dns_record():
+
+    
+
+    assert update_dns_record() is expected
+
+
+
+
+
+
+
+# def update_dns_record(cloudflare_config,
+#                       detected_ip: str,
+#                       version: str = "ipv4") -> dict:
+#     """
+#     Update the Cloudflare DNS record ('A' for IPv4, 'AAAA' for IPv6) 
+#     to the detected IP if it has changed
+
+#     Returns: 
+#         dictionary
+#     """
+
+#     api_base_url = cloudflare_config["api_base_url"]
+#     api_token    = cloudflare_config["api_token"]
+#     zone_id      = cloudflare_config["zone_id"]
+#     dns_name     = cloudflare_config["dns_name"]
+
+#     # Add comment here
+#     record_type = "A" if version.lower() == "ipv4" else "AAAA"
+
+#     # Get DNS record 
+#     dns_records_url = f"{api_base_url}/zones/{zone_id}/dns_records?name={dns_name}"
+
+#     # Cloudflare API request: authentication headers + DNS record payload
+#     header = {
+#         "Authorization": f"Bearer {api_token}",
+#         "Content-Type" : "application/json",
+#     }
+#     data = {
+#         "type"   : record_type,   # DNS record type (i.e. A, AAAA)
+#         "name"   : dns_name,      # DNS record name
+#         "content": detected_ip,   # Public IP to update
+#         "ttl"    : 60,            # TTL = Time-to-Live 
+#         "proxied": False          # Grey cloud (not proxied thru Cloudflare)
+#     }
+
+#     # Authenticate with API token
+#     resp = requests.get(dns_records_url, headers=header)     
+#     resp.raise_for_status()
+#     print(f"update_dns_record: DNS records URL: {dns_records_url}")         #####
+#     print(f"update_dns_record: DNS records JSON response:\n{json.dumps(resp.json(), indent=2)}")    #####
+
+#     # Extract the list of DNS records from the Cloudflare response
+#     records = resp.json().get("result", [])
+
+#     # Find the record that matches the desired type (A or AAAA)
+#     record = next((r for r in records if r.get("type") == record_type), None)
+
+#     if record:
+#         record_id         = record["id"]
+#         dns_record_ip     = record["content"]
+#         dns_last_modified = record["modified_on"]
+
+#         print(f"✅ Found {record_type} record: id={record_id}, ip={dns_record_ip}, modified_on={dns_last_modified}")
+#     else:
+#         print(f"❌ No {record_type} record found for {dns_name}")
+#         record_id = dns_record_ip = dns_last_modified = None
+
+#     # Update DNS record if IP has changed
+#     if dns_record_ip != detected_ip:
+#         update_url = f"{api_base_url}/zones/{zone_id}/dns_records/{record_id}"
+#         resp = requests.put(update_url, headers=header, json=data)
+#         resp.raise_for_status()
+#         print(f"update_dns_record: ✅  Updated '{dns_name}': {dns_record_ip} → {detected_ip}")       #####
+#         #return True
+#     else:
+#         print(f"update_dns_record: ℹ️  No update needed for '{dns_name}', IP unchanged")             #####
+#         #return False
+    
+#     return {
+#         "dns_name"         : dns_name,
+#         "detected_ip"      : detected_ip,
+#         "dns_last_modified": format_cloudflare_timestamp(dns_last_modified)
+#     }
+
+
