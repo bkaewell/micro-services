ffd56efbb9a3d727f2e2f0f571ac1edc096c045f
Brian Kaewell <briankaewell@gmail.com>
2025-11-04 11:40:30 -0500

    Add pytest fixture to test check_internet functionality

--- update_dns/tests/test_watchdog.py
+++ update_dns/tests/test_watchdog.py
@@ -1,15 +1,25 @@
 import pytest
-from update_dns.watchdog import check_internet
+import responses
+from update_dns.watchdog import check_internet, reset_smart_plug
 
-def test_check_internet_valid(monkeypatch):
+
+@pytest.fixture
+def mock_ping(monkeypatch):
+
+    def _mock_ping(return_code):
+        monkeypatch.setattr("os.system", lambda cmd: return_code)
+    return _mock_ping
+
+def test_check_internet_valid(mock_ping):
     # Simulate a successful ping result
-    # Mock os.system to return 0 (success)
-    monkeypatch.setattr("os.system", lambda cmd: 0)
+    mock_ping(0) # mock os.system returns 0 (success)
     host = "8.8.8.8"
     assert(check_internet(host)) is True
 
-def test_check_internet_invalid(monkeypatch):
+def test_check_internet_invalid(mock_ping):
     # Simulate a failed ping result
-    monkeypatch.setattr("os.system", lambda cmd: 1)
+    mock_ping(1) # mock os.system returns nonzero (failure)
     host = ""
     assert(check_internet(host)) is False
+
+
