b317eb9e2d6b37b6cf5a8d4c8193074de4aa3fb8
Brian Kaewell <briankaewell@gmail.com>
2025-11-17 21:54:23 -0500

    Make the logger formatting script more robust that prepends an emoji per log level

--- update_dns/src/update_dns/logger.py
+++ update_dns/src/update_dns/logger.py
@@ -1,12 +1,31 @@
-import logging
 import sys
+import logging
+
+LOG_LEVEL_EMOJIS = {
+    logging.DEBUG: "🧱",
+    logging.INFO: " 🟢",
+    logging.WARNING: " ⚠️ ",
+    logging.ERROR: "❌",
+    logging.CRITICAL: "🔥",  # For severe errors, typically logged via exception()
+}
+
+LEVEL_NAME_MAP = {
+    "WARNING": "WARN",
+    "CRITICAL": "FATAL",
+}
+
+class EmojiFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        """Formatter that prepends an emoji per log level and shortens level names"""
+        record.levelemoji = LOG_LEVEL_EMOJIS.get(record.levelno, "")
+        record.levelname = LEVEL_NAME_MAP.get(record.levelname, record.levelname)
+        return super().format(record)
 
 def setup_logging(level=logging.INFO) -> None:
-    """Configure global application logging"""
+    """Configure global application logging with emoji decorations"""
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter(
-        fmt="%(asctime)s [%(levelname)s] %(name)s:%(funcName)s:%(lineno)d → %(message)s",
-        #fmt="%(asctime)s [%(levelname)s] %(funcName)s:%(lineno)d → %(message)s",
+    formatter = EmojiFormatter(
+        fmt="%(asctime)s [%(levelname)s] %(levelemoji)s %(name)s:%(funcName)s:%(lineno)d → %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
     handler.setFormatter(formatter)
